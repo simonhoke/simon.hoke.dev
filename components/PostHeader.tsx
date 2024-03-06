05a535eea396a37bf74439fb0fac3ab592e5cc0c
Simon Hoke <simonhoke@gmail.com>
2024-03-05 21:59:35 -0800

    Fix type

--- components/PostHeader.tsx
+++ components/PostHeader.tsx
@@ -1,6 +1,5 @@
 import Avatar from 'components/AuthorAvatar'
-import CoverImage from 'components/CoverImage'
-import Date from 'components/PostDate'
+import SquareCoverImage from 'components/SquareCoverImage'
 import PostTitle from 'components/PostTitle'
 import type { Post } from 'lib/sanity.queries'
 
@@ -15,7 +14,7 @@ export default function PostHeader(
         {/*{author && <Avatar name={author.name} picture={author.picture} />}*/}
       </div>
       <div className="mb-8 sm:mx-0 md:mb-16">
-        <CoverImage title={title} image={coverImage} priority link={slug} />
+        <SquareCoverImage title={title} image={coverImage} priority slug={slug} />
       </div>
       <div className="mx-auto max-w-2xl">
         <div className="mb-6 block md:hidden">
