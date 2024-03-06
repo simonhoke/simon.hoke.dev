0eb617a4ca01e0274a72bf7375209e832bd21924
Simon Hoke <simonhoke@gmail.com>
2024-03-05 21:50:36 -0800

    Add gear to app page

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,16 +1,17 @@
-import PostPage from 'components/PostPage'
-import PreviewPostPage from 'components/PreviewPostPage'
 import { readToken } from 'lib/sanity.api'
 import {
+  getAllTools,
   getClient,
-  getPostAndMoreStories,
-  getSettings,
+  getSettings
 } from 'lib/sanity.client'
-import { Post, Settings } from 'lib/sanity.queries'
+import { Settings,Tool } from 'lib/sanity.queries'
 import { GetStaticProps } from 'next'
 import type { SharedPageProps } from 'pages/_app'
 
+import GearPlannerPage from '../components/GearPlannerPage'
+
 interface PageProps extends SharedPageProps {
+  tools: Tool[]
   settings?: Settings
 }
 
@@ -19,19 +20,22 @@ interface Query {
 }
 
 export default function ProjectSlugRoute(props: PageProps) {
-  return <h2>Hello</h2>
+  const { tools, settings } = props
+  return <GearPlannerPage tools={tools} settings={settings}></GearPlannerPage>
 }
 
 export const getServerSideProps: GetStaticProps<PageProps, Query> = async (ctx) => {
   const { draftMode = false, params = {} } = ctx
   const client = getClient(draftMode ? { token: readToken } : undefined)
 
-  const [settings] = await Promise.all([
+  const [ tools, settings] = await Promise.all([
+    getAllTools(client),
     getSettings(client),
   ])
 
   return {
     props: {
+      tools,
       settings,
     },
   }
