4454194b2deaeb0e02d2a581a5f0e7c446f6fda8
Simon Hoke <simonhoke@gmail.com>
2024-03-06 21:03:40 -0800

    Add query prompt text box

--- components/GearPlannerPage.tsx
+++ components/GearPlannerPage.tsx
@@ -6,6 +6,8 @@ import Tools from 'components/Tools'
 import * as demo from 'lib/demo.data'
 import type { Settings, Tool } from 'lib/sanity.queries'
 
+import QueryPrompt from './QueryPrompt'
+
 export interface GearPlannerPageProps {
   preview?: boolean
   loading?: boolean
@@ -24,6 +26,11 @@ export default function GearPlannerPage(props: GearPlannerPageProps) {
       <Layout preview={preview} loading={loading}>
         <Container>
           <BlogHeader title={title} description={description} level={2} />
+          <div className="mb-16 grid grid-cols-2 xl:grid-cols-4">
+            <div className="col-span-2">
+              <QueryPrompt></QueryPrompt>
+            </div>
+          </div>
           {tools.length > 0 && <Tools tools={tools} />}
         </Container>
       </Layout>
---
+++ components/QueryPrompt.tsx
@@ -0,0 +1,43 @@
+export default function QueryPrompt() {
+  return (
+    <div className="flex items-start space-x-4">
+      <div className="min-w-0 flex-1">
+        <form action="#" className="relative">
+          <div className="p-4 overflow-hidden rounded-lg shadow-sm ring-2 ring-inset ring-indigo-600">
+            <label htmlFor="comment" className="sr-only">
+              Describe your adventure
+            </label>
+            <textarea
+              rows={3}
+              name="comment"
+              id="comment"
+              className="block w-full resize-none border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6 border-transparent focus:border-transparent focus:ring-0"
+              placeholder="Describe your adventure"
+              defaultValue={''}
+            />
+
+            {/* Spacer element to match the height of the toolbar */}
+            <div className="py-2" aria-hidden="true">
+              {/* Matches height of button in toolbar (1px border + 36px content height) */}
+              <div className="py-px">
+                <div className="h-9" />
+              </div>
+            </div>
+          </div>
+
+          <div className="absolute inset-x-0 bottom-0 flex justify-between py-2 pl-3 pr-2">
+            <div className="flex items-center space-x-5"></div>
+            <div className="flex-shrink-0">
+              <button
+                type="submit"
+                className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Go
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
