ad5d65f2e132f33e53c856cf395678e0533fd054
Simon Hoke <simonhoke@gmail.com>
2024-03-08 20:38:59 -0800

    Add gear list border & title

--- components/GearList.tsx
+++ components/GearList.tsx
@@ -9,7 +9,8 @@ export default function GearList(props: GearListProps) {
   const { tools } = props
 
   return (
-    <div>
+    <div className="border-blue-500 border-2 rounded-xl p-4 m-4">
+      <h2 className="text-3xl font-bold">Gear List</h2>
       {tools.length > 0 && <Tools tools={tools} />}
     </div>
   )
