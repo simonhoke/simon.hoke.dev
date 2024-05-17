7552816a0e5fc72e16e3c189bfa86a5c19162cfc
Simon Hoke <simonhoke@gmail.com>
2024-05-17 14:00:19 -0700

    Increase image resolution

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,12 +55,12 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
                 style={{ transform: "translate3d(0, 0, 0)" }}
                 placeholder="blur"
                 blurDataURL={blurDataUrl}
-                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_1080/${public_id}.${format}`}
                 width={1920}
                 height={1080}
-                sizes="(max-width: 640px) 100vw,
-                  (max-width: 1280px) 50vw,
-                  (max-width: 1536px) 33vw,
+                sizes="(max-width: 1280px) 100vw,
+                  (max-width: 1536px) 50vw,
+                  (max-width: 1920px) 33vw,
                   25vw"
               />
             </Link>
